493bb8527b860363a0e16474f4b766f13f81ade1
nardober1st <bernardo.oechsler6@gmail.com>
2026-03-26 19:13:57 +0000

    modules relations set up

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -60,15 +60,8 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
-                // Add KMP dependencies here
 
                 implementation(projects.core.domain)
-            }
-        }
-
-        commonTest {
-            dependencies {
-                implementation(libs.kotlin.test)
             }
         }
 
@@ -77,14 +70,6 @@ kotlin {
                 // Add Android-specific dependencies here. Note that this source set depends on
                 // commonMain by default and will correctly pull the Android artifacts of any KMP
                 // dependencies declared in commonMain.
-            }
-        }
-
-        getByName("androidDeviceTest") {
-            dependencies {
-                implementation(libs.androidx.runner)
-                implementation(libs.androidx.test.core)
-                implementation(libs.androidx.junit)
             }
         }
 
@@ -98,5 +83,4 @@ kotlin {
             }
         }
     }
-
 }
